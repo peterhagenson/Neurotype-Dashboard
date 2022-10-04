ecf4bb45780c53ff88daca7a860272be3b11314d
Josh Snyder <joshua.t.snyder@gmail.com>
2022-10-04 13:17:01 -0500

    still working on the GETs

--- server/routes/institution.router.js
+++ server/routes/institution.router.js
@@ -14,11 +14,12 @@ router.get('/', (req, res) => { // Get all institutions
 
 // to get the institution related to the logged in researcher
 router.get('/researchInst', (req, res) => {
+  console.log('the user who is logged in is', req.user);
   const query = `SELECT "institution".name FROM "institution"
   JOIN "user"
   ON "institution".id = "user".inst_id
-  WHERE "user".id = 1;`;
-  pool.query(query)
+  WHERE "user".id = $1;`;
+  pool.query(query,[req.user.id])
     .then(result => {
       res.send(result.rows);
     })
